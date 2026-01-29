51bc43ada83e76a0984823db7fdc3833eb6a9477
Leslie Heintz <heintz.leslie@gmail.com>
2026-01-29 21:32:25 +0800

    Add github deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://leslieheintz.com', // Update with your actual domain
   output: 'static',
   compressHTML: true,
   integrations: [
